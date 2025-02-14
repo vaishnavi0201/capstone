1b15a9466f54507675323183e9924d52fe2462b1
username <wecp-dummy@gmail.com>
2025-02-14 13:30:00 +0000

    Landing page started

--- client/src/app/components/landing-page/landing-page.component.ts
+++ client/src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  formData: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.formData = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
   }
 
+  ngOnInit(): void {
+    console.log("working");
+  }
+
+  handleFormSubmit() {
+    if (this.formData.valid) {
+      console.log('Form submitted:', this.formData.value);
+      this.resetForm();
+    }
+  }
+  resetForm() {
+    this.formData.reset()
+  }
 }
