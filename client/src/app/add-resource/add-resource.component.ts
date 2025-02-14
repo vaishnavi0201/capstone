8b4c065ef28d55ac189b2b53552b99e8fa7f5436
username <wecp-dummy@gmail.com>
2025-02-14 07:04:57 +0000

    UI Improved: Add Resource, Update Event Status

--- client/src/app/add-resource/add-resource.component.ts
+++ client/src/app/add-resource/add-resource.component.ts
@@ -1,54 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { HttpService } from '../../services/http.service';
-// import { AuthService } from '../../services/auth.service';
-
-// @Component({
-//   selector: 'app-add-resource',
-//   templateUrl: './add-resource.component.html',
-//   styleUrls: ['./add-resource.component.scss']
-// })
-// export class AddResourceComponent implements OnInit {
-//   itemForm!: FormGroup;
-//   errorMessage: string = '';
-
-//   constructor(
-//     private fb: FormBuilder,
-//     private router: Router,
-//     private httpService: HttpService,
-//     private authService: AuthService
-//   ) {
-//     this.itemForm = this.fb.group({
-//       eventId: [null, Validators.required],
-//       type: [undefined, Validators.required],
-//       description: [undefined, Validators.required],
-//       availabilityStatus: [null, Validators.required]
-//     });
-//   }
-
-//   ngOnInit(): void {
-//     // Initialization logic, if any
-//   }
-
-//   onSubmit(): void {
-//     if (this.itemForm.invalid) {
-//       return;
-//     }
-
-//     const formData = this.itemForm.value;
-//     this.httpService.addResource(formData).subscribe(
-//       (response: any) => {
-//         this.router.navigate(['/some-success-route']); // Replace with actual success route
-//       },
-//       (error: any) => {
-//         this.errorMessage = 'Adding resource failed. Please try again.';
-//       }
-//     );
-//   }
-// }
-
-// -------------------
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -156,6 +105,12 @@ export class AddResourceComponent implements OnInit {
         this.getResources();
         this.router.navigate(['/add-resource']); // Replace with the actual success route
         this.responseMessage = 'Resource added successfully!';
+        setTimeout(() => {
+          this.responseMessage = '';
+        }, 2000);
+        setTimeout(() => {
+          this.itemForm.reset();
+        }, 0); // Ensure form reset happens after submission
       },
       (error: any) => {
         // Adding user-friendly messages when the form submission fails due to validation errors
