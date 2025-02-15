0dd9b33954064c4b362b83f598e903ed72d6eb9e
username <wecp-dummy@gmail.com>
2025-02-15 04:52:56 +0000

    Landing Page: Card Shadow, and some other fixes

--- client/src/app/components/landing-page/landing-page.component.ts
+++ client/src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swiper from 'swiper';
 
@@ -7,8 +7,22 @@ import Swiper from 'swiper';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
-  
+export class LandingPageComponent implements OnInit, AfterViewInit {
+
+  formData: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formData = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    console.log("working");
+  }
+
   ngAfterViewInit() {
     new Swiper('.swiper-container', {
       loop: true,
@@ -21,27 +35,11 @@ export class LandingPageComponent implements OnInit {
       },
     });
   }
-  
-
-  formData: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.formData = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required]],
-      message: ['', Validators.required]
-    });
-  }
-
-  ngOnInit(): void {
-    console.log("working");
-  }
 
   handleFormSubmit() {
     if (this.formData.valid) {
       console.log('Form submitted:', this.formData.value);
-      this.formData.reset()
+      this.formData.reset();
     }
   }
-
 }
